f6a0d7c8cc744e5563e2f529525504e7d0b24c9b
Ryan Jones <ryan.nolan.jones.53@gmail.com>
2015-04-30 20:59:27 -0500

    improvement on TLS

--- src/mp5/TLS.java
+++ src/mp5/TLS.java
@@ -27,11 +27,12 @@ public class TLS extends Thread implements Observer{
 	public void nsState(lightState newState){ nsState = newState;}
 
 	public void run(){
-		boolean statusChanged = false;
+		
 		while(true){
 			time = stopWatch.elapsedTime();
-			if(nsState==lightState.red&&ewState==lightState.green&&time>=(30.0)){
+			if((nsState==lightState.red&&ewState==lightState.green)&&(time>=(30.0))){
 				ewState = lightState.amber;
+				System.out.println();
 				System.out.println("East-South Stoplight: Amber");
 				stopWatch.stop();
 				stopWatch.start();
@@ -39,15 +40,16 @@ public class TLS extends Thread implements Observer{
 			else if(ewState==lightState.amber && nsState == lightState.red&&time>=(5.0)){
 				nsState = lightState.green;
 				ewState = lightState.red;
+				System.out.println();
 				System.out.println("North-South Stoplight: Green");
 				System.out.println("East-West Stoplight: Red");
 				stopWatch.stop();
 				stopWatch.start();
-				statusChanged = false;
 			}
 				
-			if((nsState==lightState.green&&ewState==lightState.triggered)&&time>=(30.0)){
+			else if((nsState==lightState.green&&ewState==lightState.triggered)&&time>=(30.0)){
 				nsState = lightState.amber;
+				System.out.println();
 				System.out.println("North-South Stoplight: Amber");
 				stopWatch.stop();
 				stopWatch.start();
@@ -55,18 +57,13 @@ public class TLS extends Thread implements Observer{
 			else if(nsState==lightState.amber && time>=5.0 && ewState == lightState.triggered){
 				ewState = lightState.green;
 				nsState = lightState.red;
+				System.out.println();
 				System.out.println("East-West Stoplight: Green");
 				System.out.println("North-South Stoplight: Red");
 				stopWatch.stop();
 				stopWatch.start();
 			}
-			else if(ewState == lightState.triggered && statusChanged == false){
-				System.out.println();
-				System.out.println("East-West Stoplight: car detected");
-				statusChanged = true;
-			}
-			
-			
+		
 			
 		
 		/*if(time%5==0)
@@ -78,8 +75,11 @@ public class TLS extends Thread implements Observer{
 	
 	public synchronized void update(Boolean carSensor) {
 			
-		if(carSensor==true)
+		if(carSensor==true&&ewState==lightState.red){
+				System.out.println();
+				System.out.println("East-West Stoplight: car detected");
 				ewState = lightState.triggered;		
+		}
 		
 	}
 	
